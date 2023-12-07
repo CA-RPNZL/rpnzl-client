459b4532b31ae0adca7ba92915dc429337aa3768
Helen T <helen-thai@hotmail.com>
2023-12-07 19:46:25 +1100

    fix: re-add booking route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Booking from './pages/Booking';
 import ContactUs from './pages/ContactUs';
 import Homepage from './pages/Homepage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />}/>
           <Route path="/contactus" element={<ContactUs />}/>
+          <Route path="/booking" element={<Booking />}/>
         </Routes>
         <Footer />
       </BrowserRouter>
