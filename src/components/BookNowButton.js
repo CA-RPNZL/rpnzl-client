ee054adb866fa0794722737d6502d9f4c2b7f461
Helen T <helen-thai@hotmail.com>
2023-12-14 22:39:05 +1100

    fix: Book Now button doesn't reload page

--- src/components/BookNowButton.js
+++ src/components/BookNowButton.js
@@ -1,10 +1,17 @@
 import Button from 'react-bootstrap/Button';
 import '../styling/BookNowButton.css';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function BookNowButton() {
+    let navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/booking")
+    };
+
     return (
         <div id="bookNowBtnContainer">
-            <Button href="/booking" className="bookNowButton">BOOK NOW</Button>
+            <Button onClick={handleClick} className="bookNowButton">BOOK NOW</Button>
         </div>
     )
 }
