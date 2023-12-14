58f6dedd5321fa105038257af408772af1b9ed09
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-12-14 23:06:21 +1100

    feat: Navigate tabs to cycle Admin Portal, cleaned up code

--- src/pages/AdminPortal.jsx
+++ src/pages/AdminPortal.jsx
@@ -1,36 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
 import AppointmentsTab from '../components/AdminAppointmentsTab'; 
 import UsersTab from '../components/AdminUsersTab';
-import "../styling/AdminPortal.css"
 import ServicesTab from '../components/AdminServicesTab';
-
+import "../styling/AdminPortal.css";
 
 function AdminPortal() {
     const [appointments, setAppointments] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [services, setServices] = useState([]);
     const [activeTab, setActiveTab] = useState('appointments');
 
     useEffect(() => {
         const fetchAppointments = async () => {
             try {
-                // Fetch data from API
-                let response = await fetch(process.env.REACT_APP_API + "/appointments")
-                // Save data as json
+                let response = await fetch(process.env.REACT_APP_API + "/appointments");
                 const responseData = await response.json();
-                // Update state
                 setAppointments(responseData);
-                console.log(response)
+                console.log(response);
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
 
-        fetchAppointments();
-    }, []);
-
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
         const fetchUsers = async () => {
             try {
                 let response = await fetch(process.env.REACT_APP_API + "/users");
@@ -39,14 +30,8 @@ function AdminPortal() {
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
 
-        fetchUsers();
-    }, []);
-
-    const [services, setServices] = useState([]);
-
-    useEffect(() => {
         const fetchServices = async () => {
             try {
                 let response = await fetch(process.env.REACT_APP_API + "/services");
@@ -55,63 +40,79 @@ function AdminPortal() {
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
 
+        fetchAppointments();
+        fetchUsers();
         fetchServices();
     }, []);
-    
 
-    
+    const handleTabChange = (tab) => {
+        setActiveTab(tab);
+    };
+
     return (
         <div>
-            <div id="appointments">
-                <h1>Appointments</h1>
-                <div id="appointmentcontainer">
-                    {/* Populate cards if appointmentss contains a value */}
-                {appointments.length > 0 && 
-                appointments.map(appointments => (
-                    <AppointmentsTab
-                        key={appointments._id}
-                        client={appointments.client}
-                        hairstylist={appointments.hairstylist}
-                        service={appointments.service}
-                        duration={appointments.duration}
-                    />
-                ))}
-                </div>
-            </div>
-            <div id="users">
-                <h1>Users</h1>
-                <div id="user-container">
-                    {users.map((user) => (
-                        <UsersTab
-                            key={user._id}
-                            firstName={user.firstName}
-                            lastName={user.lastName}
-                            mobileNumber={user.mobileNumber}
-                            email={user.email}
-                            isHairstylist={user.is_hairstylist}
-                        />
-                    ))}
-                </div>
-            </div>
-            <div id="services">
-                <h1>Services</h1>
-                <div id="service-container">
-                    {services.map((service) => (
-                        <ServicesTab
-                            key={service._id}
-                            serviceName={service.name}
-                            description={service.description}
-                            price={service.price}
-                            duration={service.duration}
-                            />
-                    ))}
-                </div>
+            <div id="tab-buttons">
+                <button onClick={() => handleTabChange('appointments')}>Appointments</button>
+                <button onClick={() => handleTabChange('users')}>Users</button>
+                <button onClick={() => handleTabChange('services')}>Services</button>
             </div>
 
+            {activeTab === 'appointments' && (
+                <div id="appointments">
+                    <h1>Appointments</h1>
+                    <div id="appointmentcontainer">
+                        {appointments.length > 0 &&
+                            appointments.map((appointment) => (
+                                <AppointmentsTab
+                                    key={appointment._id}
+                                    client={appointment.client}
+                                    hairstylist={appointment.hairstylist}
+                                    service={appointment.service}
+                                    duration={appointment.duration}
+                                />
+                            ))}
+                    </div>
+                </div>
+            )}
+
+            {activeTab === 'users' && (
+                <div id="users">
+                    <h1>Users</h1>
+                    <div id="user-container">
+                        {users.map((user) => (
+                            <UsersTab
+                                key={user._id}
+                                firstName={user.firstName}
+                                lastName={user.lastName}
+                                mobileNumber={user.mobileNumber}
+                                email={user.email}
+                                isHairstylist={user.is_hairstylist}
+                            />
+                        ))}
+                    </div>
+                </div>
+            )}
+
+            {activeTab === 'services' && (
+                <div id="services">
+                    <h1>Services</h1>
+                    <div id="service-container">
+                        {services.map((service) => (
+                            <ServicesTab
+                                key={service._id}
+                                serviceName={service.name}
+                                description={service.description}
+                                price={service.price}
+                                duration={service.duration}
+                            />
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
-        
-    )
+    );
 }
+
 export default AdminPortal;
